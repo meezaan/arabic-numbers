46aa5eaacd48d695f413cd727d43464715950a60
Meezaan-ud-Din <meezaan@vesica.co.uk>
2017-08-31 12:14:12 +0100

    Updated, commented, unit tested

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
+
+
+بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ
+
 # Arabic Numbers
+This library helps convert between latin and arabic numerals and vice versa.
+
+## Installation
+To install:
+
+```
+composer require meezaan/arabic-numbers
+```
+
+## Usage
+To use the library in your apps, make sure the composer autoload file is already included. Then:
+
+```php
+use ArabicNumbers\Numbers;
+
+// Latin to Arabic
+$arabic = Numbers::latinToArabic(157); // returns ١٥٧
+
+// Arabic to latin
+$latin - Numbers::arabicToLatin(١٥٧); // returns 157
+
+// Arabic to HTML
+$html = Numbers::ArabicToHtml(١٥٧); // returns &#1633;&#1637;&#1639;
+```
+
+## Unit Tests
+These are included in the tests folder. To run them, make sure you've already run composer install. Then:
+```
+php vendor/bin/phpunit --bootstrap vendor/autoload.php tests/
+```
+
+Happy days.
--- src/ArabicNumbers/Numbers.php
+++ src/ArabicNumbers/Numbers.php
@@ -3,17 +3,60 @@ namespace ArabicNumbers;
 
 class Numbers
 {
+    /**
+     * Converts Latin numerals to Arabic numerals
+     * @var $number integer
+     * @return string
+     **/
     public static function latinToArabic($number)
     {
-        $number = (string) $number;
-        foreach (self::getArabicNumerals() as $latin => $arr) {
-            $number = str_replace($latin, $arr['arabic'], $number);
-        }
+        return self::convert('latin', 'arabic', $number);
 
-        return $number;
+    }
+    
+    /**
+     * Converts Arabic numerals to Latin numerals
+     * @var $number string
+     * @return string
+     **/
+    public static function arabicToLatin($number)
+    {
+        return self::convert('arabic', 'latin', $number);
 
     }
 
+    /**
+     * Converts Arabic numerals to their HTML equivalent
+     * @var $number string
+     * @return string
+     **/
+    public static function arabicToHtml($number)
+    {
+        return self::convert('arabic', 'html', $number);
+
+    }
+
+    /**
+     * Does the actual conversions between arabic and latin
+     * @var $from string 'arabic' or 'latin'
+     * @var $to string 'arabic' or 'latin' or 'html'
+     * @var $number string
+     * @return string
+     **/ 
+    private static function convert($from, $to, $number)
+    {
+        $number = (string) $number;
+        foreach (self::getArabicNumerals() as $latin => $arr) {
+            $number = str_replace($arr[$from], $arr[$to], $number);
+        }
+
+        return $number;
+                
+    }
+
+    /**
+     * Returns an array of arabic and latin numerals and the arabic html equivalent
+     **/
     public static function getArabicNumerals()
     {
         return [
